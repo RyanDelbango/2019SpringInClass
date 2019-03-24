213dc860a41a35439f060c909099367bb59c1a9a
Ryan Delbango <Ryan.Delbango@gmail.com>
2019-03-24 14:53:06 -0400

    Navigation & Components

---
+++ src/views/MyFriends.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <h1>My Friends Page</h1>
+    <ul>
+        <li>Friend 1</li>
+        <li>Friend2</li>
+        <li>Friend3</li>
+        <li>Friend4</li>
+        <li>Friend 5</li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
