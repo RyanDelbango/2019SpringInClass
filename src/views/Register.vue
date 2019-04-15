58894d0d03801c9937611694398850511ef243e9
Ryan Delbango <Ryan.Delbango@gmail.com>
2019-04-15 10:25:01 -0400

    fixed bug in error handling

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -80,7 +80,7 @@ export default {
               toastr.success("You've registered successfully!")
             } catch (error) {
               Globals.errors.push(error);
-              toastr.error(error.msg);
+              toastr.error(error.message);
             }
         }
     }
